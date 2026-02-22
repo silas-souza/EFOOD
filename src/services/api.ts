23ac6931997d582b90dee75f9359102cb8bd66e2
silas souza <silas.souzameister1@gmail.com>
2026-02-21 21:05:32 -0300

    fix: contornando erro de CORS com proxy para carregar dados

--- src/services/api.ts
+++ src/services/api.ts
@@ -5,7 +5,7 @@ import Checkout from '../models/Checkout'
 
 const api = createApi({
   baseQuery: fetchBaseQuery({
-    baseUrl: 'https://ebac-fake-api.vercel.app/api/efood/'
+    baseUrl: 'https://cors-proxy.org/?url=https://ebac-fake-api.vercel.app/api/efood/'
   }),
   endpoints: (builder) => ({
     getCardapio: builder.query<Cardapio[], void>({
